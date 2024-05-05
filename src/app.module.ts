2be84e929e0d720dad1e476c414f456eb783cf41
Kostiantyn Tselyshchev <kostiantyn_tselyshchev@epam.com>
2024-05-05 17:13:05 +0200

    feat: complete task-8

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 
@@ -8,6 +9,16 @@ import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     AuthModule,
     CartModule,
     OrderModule,
